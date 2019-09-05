187b929126b9423771e4cfe3028141c0f19dce6d
Suren1987 <46677019+Suren1987@users.noreply.github.com>
2019-09-05 16:00:10 -0400

    week 2 excersie

---
+++ week2_practice.py
@@ -0,0 +1,8 @@
+#The codes below calculate the area and perimeter of a circle
+pi = 3.14 
+r =2.0
+area = pi *(r**2)
+perim = 2.0*pi*r 
+print "Given r: ", r
+print " Area is: ", area
+print " Perimeter is: ", perim
